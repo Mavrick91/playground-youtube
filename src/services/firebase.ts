c6b982834fe0abb1340c6899afe424d7bb0ea903
Mavrick91 <ray.mavrick@gmail.com>
2023-12-05 14:06:05 +0800

    Update Firebase initialization and analytics

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,23 @@
-import { initializeApp } from "firebase/app";
+import { getApps, initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDfgwgTK_zy7-n54DP0iIrKnHK98iW9oXw",
-    authDomain: "vidventure-c31d0.firebaseapp.com",
-    projectId: "vidventure-c31d0",
-    storageBucket: "vidventure-c31d0.appspot.com",
-    messagingSenderId: "902832946249",
-    appId: "1:902832946249:web:2a91d8bbb5ca23dcf31398",
-    measurementId: "G-QVXME0ECPH"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const apps = getApps();
+const app = !apps.length ? initializeApp(firebaseConfig) : apps[0];
+
+let analytics;
+
+if (process.env.NODE_ENV === 'production') {
+    analytics = getAnalytics(app);
+}
 
 export { app }
