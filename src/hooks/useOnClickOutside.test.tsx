865ab0e395fc99503935e8535054ae488aa4cb06
Mavrick91 <ray.mavrick@gmail.com>
2023-12-05 20:06:07 +0800

    Add useOnClickOutside hook and its test file

---
+++ src/hooks/useOnClickOutside.test.tsx
@@ -0,0 +1,48 @@
+// FILEPATH: /Users/mavrickduchamp/programming/perso/js/playground/src/hooks/useOnClickOutside.test.tsx
+import React, { useRef } from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import useOnClickOutside from './useOnClickOutside';
+
+const handler = jest.fn();
+
+const TestComponent = () => {
+  const ref = useRef(null);
+  useOnClickOutside(ref, handler);
+
+  return (
+    <div>
+      <div ref={ref}>Inside</div>
+      <div>Outside</div>
+    </div>
+  );
+};
+
+describe('useOnClickOutside', () => {
+  afterEach(() => {
+    handler.mockReset();
+  });
+
+  it('calls the handler when a click is outside the ref', () => {
+    const { getByText } = render(<TestComponent />);
+    fireEvent.mouseDown(getByText('Outside'));
+    expect(handler).toHaveBeenCalled();
+  });
+
+  it('does not call the handler when a click is inside the ref', () => {
+    const { getByText } = render(<TestComponent />);
+    fireEvent.click(getByText('Inside'));
+    expect(handler).not.toHaveBeenCalled();
+  });
+
+  it('calls the handler when the Escape key is pressed', () => {
+    render(<TestComponent />);
+    fireEvent.keyDown(document, { key: 'Escape' });
+    expect(handler).toHaveBeenCalled();
+  });
+
+  it('does not call the handler when a key other than Escape is pressed', () => {
+    render(<TestComponent />);
+    fireEvent.keyDown(document, { key: 'Enter' });
+    expect(handler).not.toHaveBeenCalled();
+  });
+});
--- src/hooks/useOnClickOutside.tsx
+++ src/hooks/useOnClickOutside.tsx
@@ -5,17 +5,31 @@ const useOnClickOutside = (
   handler: () => void
 ) => {
   useEffect(() => {
-    const listener = (event: MouseEvent) => {
-      if (!ref.current || ref.current.contains(event.target as Node)) {
+    const listener = (event: MouseEvent | KeyboardEvent) => {
+      // Close the modal if a click is outside the modal or 'Escape' key is pressed
+      if (
+        (!ref.current || ref.current.contains(event.target as Node)) &&
+        event.type !== 'keydown'
+      ) {
         return;
       }
+
+      if (
+        event.type === 'keydown' &&
+        (event as KeyboardEvent).key !== 'Escape'
+      ) {
+        return;
+      }
+
       handler();
     };
 
     document.addEventListener('mousedown', listener);
+    document.addEventListener('keydown', listener);
 
     return () => {
       document.removeEventListener('mousedown', listener);
+      document.removeEventListener('keydown', listener);
     };
   }, [ref, handler]);
 };
