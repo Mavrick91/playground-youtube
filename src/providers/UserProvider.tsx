b9754d77a30552274933bba4bb22d4e68e2a4927
Mavrick91 <ray.mavrick@gmail.com>
2023-12-22 15:35:37 +0800

    Refactor UserProvider component to include usePathname hook

--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -1,17 +1,22 @@
 'use client';
 
-import React, { createContext, useState, useEffect, useContext } from 'react';
-import { auth, onAuthStateChanged, User } from '~/services/firebase';
+import { usePathname } from 'next/navigation';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { User, auth, onAuthStateChanged } from '~/services/firebase';
 
 const UserContext = createContext<User | null>(null);
 
 export const UserProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
+  const pathname = usePathname();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, firebaseUser => {
-      if (firebaseUser) {
+      if (
+        (firebaseUser && firebaseUser.emailVerified) ||
+        pathname.includes('/auth/action')
+      ) {
         console.log('🚀 ~ unsubscribe ~ firebaseUser:', firebaseUser);
         setUser(firebaseUser);
       } else {
@@ -21,7 +26,7 @@ export const UserProvider = ({ children }: { children: React.ReactNode }) => {
     });
 
     return () => unsubscribe();
-  }, []);
+  }, [pathname]);
 
   if (loading) {
     return null;
