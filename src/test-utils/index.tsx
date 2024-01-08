9bb6ff9a61e527e137dfed245281fc8efe4e2e41
Mavrick91 <ray.mavrick@gmail.com>
2024-01-08 19:34:03 +0800

    Refactor test-utils module

---
+++ src/test-utils/index.tsx
@@ -0,0 +1,59 @@
+import { RenderOptions, render } from '@testing-library/react';
+import React, { ComponentType } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+export function createDefaultQueryResponse(overrides = {}) {
+  return {
+    data: undefined,
+    error: null,
+    isError: false,
+    isLoading: false,
+    isSuccess: false,
+    isFetching: false,
+    isIdle: true,
+    status: 'idle',
+    dataUpdatedAt: 0,
+    errorUpdatedAt: 0,
+    failureCount: 0,
+    isFetched: false,
+    isFetchedAfterMount: false,
+    isLoadingError: false,
+    isRefetchError: false,
+    isRefetching: false,
+    isStale: true,
+    refetch: async () => {},
+    remove: () => {},
+    errorUpdateCount: 0,
+    isPlaceholderData: false,
+    isPreviousData: false,
+    ...overrides,
+  };
+}
+
+export const AllTheProviders = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // ✅ turns retries off
+        retry: false,
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
+  render(ui, { wrapper: AllTheProviders as ComponentType, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
