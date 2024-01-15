2069264c232b1861054055d9d9d99b084e57b0e7
Mavrick91 <ray.mavrick@gmail.com>
2024-01-15 14:46:53 +0800

    Refactor WatchPage component to fetch video, channel, and comment data

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -2,53 +2,77 @@ import { cookies } from 'next/headers';
 import React from 'react';
 import data from './data.json';
 import YoutubePlayer from '~/components/YoutubePlayer';
-import { YoutubeVideo } from '~/types/videos';
 import DescriptionVideo from '~/components/DescriptionVideo';
+import CommentSection from '~/components/CommentSection';
+import { createURL } from '~/lib/url-utils';
+import { youtube_v3 } from 'googleapis';
+import { Separator } from '~/components/Separator';
 
 async function getData(
   auth_token: string | undefined,
   searchParams: Record<string, string>
-): Promise<YoutubeVideo> {
-  const url = new URL('/api/youtube/videos', process.env.NEXT_PUBLIC_APP_URL);
-  const params = new URLSearchParams(searchParams);
-  url.search = params.toString();
-
-  const res = await fetch(url.toString(), {
+): Promise<{
+  video: youtube_v3.Schema$VideoListResponse;
+  channel: youtube_v3.Schema$ChannelListResponse;
+  commentThreads: youtube_v3.Schema$CommentThreadListResponse;
+}> {
+  const options = {
     cache: 'no-cache',
     headers: {
       Cookie: `auth_token=${auth_token}`,
     },
-  });
+  } as RequestInit;
 
-  const responseData = await res.json();
+  const videoUrl = createURL('/api/youtube/videos', { v: searchParams.v });
+  const commentThreadsUrl = createURL('/api/youtube/comments', { videoId: searchParams.v });
 
-  if (!res.ok) {
-    throw new Error(responseData.message);
+  const [videoRes, commentThreadsRes] = await Promise.all([
+    fetch(videoUrl, options),
+    fetch(commentThreadsUrl, options),
+  ]);
+
+  const [videoData, commentThreadsData] = await Promise.all([videoRes.json(), commentThreadsRes.json()]);
+
+  if (!videoRes.ok) {
+    throw new Error(videoData.message);
   }
 
-  return responseData;
+  if (!commentThreadsRes.ok) {
+    throw new Error(commentThreadsData.message);
+  }
+
+  const channelId = videoData.items[0].snippet.channelId;
+  const channelUrl = createURL('/api/youtube/channels', { id: channelId });
+  const channelRes = await fetch(channelUrl, options);
+  const channelData = await channelRes.json();
+
+  if (!channelRes.ok) {
+    throw new Error(channelData.message);
+  }
+
+  return { video: videoData, channel: channelData, commentThreads: commentThreadsData };
 }
 
-export default async function WatchPage({
-  searchParams,
-}: {
-  searchParams: Record<string, string>;
-}) {
+export default async function WatchPage({ searchParams }: { searchParams: Record<string, string> }) {
   const auth_token = cookies().get('auth_token')?.value;
   // const data = await getData(auth_token, searchParams);
   // console.log('🚀 ~ data:', JSON.stringify(data));
-  const video = data.items[0];
+  const video = data.video.items?.[0];
+  const channel = data.channel.items?.[0];
+  const commentThreads = data.commentThreads;
 
   return (
     <div className="overflow-auto">
       <div className="flex pt-6">
         <div className="pr-6 w-9/12">
-          <YoutubePlayer video={data.items[0]} />
+          <YoutubePlayer video={video} channel={channel} />
           <DescriptionVideo
-            description={video.snippet.description}
-            publishedAt={video.snippet.publishedAt}
-            viewCount={video.statistics.viewCount}
+            description={video?.snippet?.description || ''}
+            publishedAt={video?.snippet?.publishedAt || ''}
+            viewCount={video?.statistics?.viewCount || ''}
           />
+          <Separator className="my-8" />
+          <CommentSection commentCount={video?.statistics?.commentCount || ''} commentThreads={commentThreads} />
         </div>
         <div className="border border-red-500 w-3/12">test</div>
       </div>
