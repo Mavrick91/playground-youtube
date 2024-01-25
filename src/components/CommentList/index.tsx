5e956e736d4fbb8b818e88fe22b4f933e7420c39
Mavrick91 <ray.mavrick@gmail.com>
2024-01-25 13:26:16 +0700

    Refactor CommentList component

--- src/components/CommentList/index.tsx
+++ src/components/CommentList/index.tsx
@@ -18,25 +18,26 @@ export default function CommentList() {
           </div>
         </div>
       )}
-      <div className={cn("lex flex-col gap-y-2", {
-        'opacity-50': isFetching,
-      })}>
+      <div
+        className={cn('lex flex-col gap-y-2', {
+          'opacity-50': isFetching,
+        })}
+      >
+        {data?.items?.map(item => {
+          const parentCommentId = item.id;
 
-      {data?.items?.map(item => {
-        const parentCommentId = item.id;
-        
-        return (
-          <div className="flex flex-col mb-2" key={parentCommentId}>
-            <CommentItem comment={item} parentCommentId={parentCommentId} />
+          return (
+            <div className="flex flex-col mb-2" key={parentCommentId}>
+              <CommentItem comment={item} parentCommentId={parentCommentId} />
 
-            <div className="text-gray-700 text-sm font-medium ml-16">
-              {(item.snippet?.totalReplyCount || 0) >= 1 && (
-                <CommentAnswers parentCommentId={parentCommentId} replyCount={item.snippet?.totalReplyCount} />
+              <div className="text-gray-700 text-sm font-medium ml-16">
+                {(item.snippet?.totalReplyCount || 0) >= 1 && (
+                  <CommentAnswers parentCommentId={parentCommentId} replyCount={item.snippet?.totalReplyCount} />
                 )}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
       </div>
     </div>
   );
