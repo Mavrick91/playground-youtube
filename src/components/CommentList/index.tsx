85eef0a327feae52e57e37dd3a695a70ad352bad
Mavrick91 <ray.mavrick@gmail.com>
2024-01-25 11:56:43 +0700

    Add cn import and update CommentList layout

--- src/components/CommentList/index.tsx
+++ src/components/CommentList/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useOrderComments } from '~/providers/OrderCommentsProvider';
+import { cn } from '~/lib/utils';
 import Loading from '../shared/Loading';
 import CommentAnswers from './CommentAnswers';
 import CommentItem from './CommentItem';
@@ -9,29 +10,34 @@ export default function CommentList() {
   const { data, isFetching } = useOrderComments();
 
   return (
-    <div className="flex flex-col gap-y-4 relative" data-testid="comment-list">
-      {true && (
-        <div className="absolute inset-0 bg-gradient-to-r from-white/50 from-10% to-transparent z-50">
+    <div className="frelative" data-testid="comment-list">
+      {isFetching && (
+        <div className="absolute inset-0">
           <div className="mt-20">
             <Loading type="puff" size={40} />
           </div>
         </div>
       )}
+      <div className={cn("lex flex-col gap-y-2", {
+        'opacity-50': isFetching,
+      })}>
+
       {data?.items?.map(item => {
         const parentCommentId = item.id;
-
+        
         return (
-          <div className="flex flex-col" key={parentCommentId}>
+          <div className="flex flex-col mb-2" key={parentCommentId}>
             <CommentItem comment={item} parentCommentId={parentCommentId} />
 
             <div className="text-gray-700 text-sm font-medium ml-16">
               {(item.snippet?.totalReplyCount || 0) >= 1 && (
                 <CommentAnswers parentCommentId={parentCommentId} replyCount={item.snippet?.totalReplyCount} />
-              )}
+                )}
             </div>
           </div>
         );
       })}
+      </div>
     </div>
   );
 }
