489991207b329e7e79a1dfa1453786e68129b29a
Mavrick91 <ray.mavrick@gmail.com>
2024-01-25 13:26:44 +0700

    Fix typo in channel_tabs import and update Tabs component props

--- src/components/Tabs/Tabs.test.tsx
+++ src/components/Tabs/Tabs.test.tsx
@@ -1,6 +1,6 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { usePathname, useRouter } from 'next/navigation';
-import { CHANNEL_TABS } from '~/constants/channe_tabs';
+import { CHANNEL_TABS } from '~/constants/channel_tabs';
 import Tabs from '.';
 
 jest.mock('next/navigation', () => ({
@@ -8,16 +8,18 @@ jest.mock('next/navigation', () => ({
   usePathname: jest.fn(),
 }));
 
+const channelId = 'UCRE9cpOPYEBMx4V6vTzfOWQ';
+
 describe('Tabs', () => {
   const mockPush = jest.fn();
   (useRouter as jest.Mock).mockReturnValue({
     push: mockPush,
   });
 
-  (usePathname as jest.Mock).mockReturnValue(`/channel/UCRE9cpOPYEBMx4V6vTzfOWQ/${CHANNEL_TABS[1].id}`);
+  (usePathname as jest.Mock).mockReturnValue(`/channel/${channelId}/${CHANNEL_TABS[1].id}`);
 
   it('renders all tabs', () => {
-    render(<Tabs activeTab="videos" />);
+    render(<Tabs channelId={channelId} activeTab="videos" />);
 
     CHANNEL_TABS.forEach(tab => {
       expect(screen.getByText(tab.title)).toBeInTheDocument();
@@ -25,18 +27,18 @@ describe('Tabs', () => {
   });
 
   it('sets the first corresponding tab as active', () => {
-    render(<Tabs activeTab="playlist" />);
+    render(<Tabs channelId={channelId} activeTab="playlist" />);
 
     expect(screen.getByText(CHANNEL_TABS[1].title)).toHaveClass('text-opacity-100');
   });
 
   it('updates the active tab when a tab is clicked', () => {
-    render(<Tabs activeTab="videos" />);
+    render(<Tabs channelId={channelId} activeTab="videos" />);
 
+    const firstTab = screen.getByText(CHANNEL_TABS[0].title);
     const secondTab = screen.getByText(CHANNEL_TABS[1].title);
-    fireEvent.click(secondTab);
 
-    expect(secondTab).toHaveClass('text-opacity-100');
-    expect(mockPush).toHaveBeenCalled();
+    expect(firstTab).toHaveAttribute('href', `/channel/${channelId}/${CHANNEL_TABS[0].id}`);
+    expect(secondTab).toHaveAttribute('href', `/channel/${channelId}/${CHANNEL_TABS[1].id}`);
   });
 });
