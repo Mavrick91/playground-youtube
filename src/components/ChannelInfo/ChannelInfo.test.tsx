1cd88ef24673b31b65afa6a92f29b6280317e854
Mavrick91 <ray.mavrick@gmail.com>
2024-03-07 18:09:49 +0800

    Update ChannelInfo tests, add ChannelSubscribeButton tests

--- src/components/ChannelInfo/ChannelInfo.test.tsx
+++ src/components/ChannelInfo/ChannelInfo.test.tsx
@@ -1,7 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { youtube_v3 } from 'googleapis';
 import { formatNumber } from '~/lib/utils';
+import ChannelSubscribeButton from '~/components/shared/ChannelSubscribeButton';
+import { useUser } from '~/providers/UserProvider';
 import ChannelInfo from '.';
+
+jest.mock('~/providers/UserProvider', () => ({
+  useUser: jest.fn().mockReturnValue({
+    user: {
+      id: '1',
+    },
+  }),
+}));
+
+jest.mock('~/components/shared/ChannelSubscribeButton', () => jest.fn(() => null));
 
 const channel: youtube_v3.Schema$Channel = {
   id: 'test_channel_id',
@@ -25,15 +37,43 @@ const videoSubscription: youtube_v3.Schema$SubscriptionListResponse = {
   items: [],
 };
 
-test('renders channel info correctly', () => {
-  render(<ChannelInfo channel={channel} videoSubscription={videoSubscription} />);
+describe('ChannelSubscribeButton', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-  expect(screen.getByText('Test Channel')).toBeInTheDocument();
-  expect(screen.getByText('test_channel_url')).toBeInTheDocument();
-  expect(screen.getByText(/1.0 K Subscriber/)).toBeInTheDocument();
-  expect(screen.getByText(`${formatNumber('100')} videos`)).toBeInTheDocument();
-  expect(screen.getByText('This is a test channel')).toBeInTheDocument();
+  it('renders channel info correctly', () => {
+    render(<ChannelInfo channel={channel} videoSubscription={videoSubscription} />);
 
-  const channelImage = screen.getByAltText('channel banner') as HTMLImageElement;
-  expect(channelImage.src).toContain(encodeURIComponent('http://test.com'));
+    expect(screen.getByText('Test Channel')).toBeInTheDocument();
+    expect(screen.getByText('test_channel_url')).toBeInTheDocument();
+    expect(screen.getByText(/1.0 K Subscriber/)).toBeInTheDocument();
+    expect(screen.getByText(`${formatNumber('100')} videos`)).toBeInTheDocument();
+    expect(screen.getByText('This is a test channel')).toBeInTheDocument();
+
+    const channelImage = screen.getByAltText('channel banner') as HTMLImageElement;
+    expect(channelImage.src).toContain(encodeURIComponent('http://test.com'));
+  });
+
+  it('calls ChannelSubscribeButton with correct props', () => {
+    render(<ChannelInfo channel={channel} videoSubscription={videoSubscription} />);
+
+    expect(ChannelSubscribeButton).toHaveBeenCalledWith(
+      expect.objectContaining({
+        channelId: channel.id,
+        videoSubscription: { items: videoSubscription.items },
+      }),
+      {}
+    );
+  });
+
+  it('does not render ChannelSubscribeButton if channel belongs to current user', () => {
+    (useUser as jest.Mock).mockReturnValue({
+      user: { id: 'test_channel_id' },
+    });
+
+    render(<ChannelInfo channel={channel} videoSubscription={videoSubscription} />);
+
+    expect(ChannelSubscribeButton).not.toHaveBeenCalled();
+  });
 });
