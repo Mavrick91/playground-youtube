29454d63397624b06b7356a3cca86e33d2e3ee42
Mavrick91 <ray.mavrick@gmail.com>
2024-03-08 15:03:23 +0800

    Unused imports were removed and some unnecessary code was cleaned up.

--- src/app/channel/_components/TabComponents/Videos/index.tsx
+++ src/app/channel/_components/TabComponents/Videos/index.tsx
@@ -1,22 +1,14 @@
 import MaxWidthWrapper from '~/components/MaxWidthWrapper';
-import { getActivities, getActivitiesDetails, GetActivitiesDetailsReturn } from '~/services/activityService';
-import { youtube_v3 } from 'googleapis';
+import { getActivitiesDetails, GetActivitiesDetailsReturn } from '~/services/activityService';
 import getQueryClient from '~/getQueryClient';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import ChannelVideos from '~/app/channel/_components/TabComponents/Videos/ChannelVideos';
 import { Suspense } from 'react';
 import VideoLoading from '~/app/channel/_components/TabComponents/Videos/loading';
-import { getVideoDetailsWithChannels } from '~/services/videoService';
-import { VideoListResponseWithChannel } from '~/types/searchVideos';
 
 type Props = {
   channelId: string;
 };
-
-function getVideoIdFromItemContentDetails(item: youtube_v3.Schema$Activity) {
-  if (item.contentDetails?.upload?.videoId) return item.contentDetails.upload.videoId;
-  return item.contentDetails?.playlistItem?.resourceId?.videoId;
-}
 
 async function VideoPage({ channelId }: Props) {
   const queryClient = getQueryClient();
