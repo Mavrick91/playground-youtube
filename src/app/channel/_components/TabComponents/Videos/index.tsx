d4e9a98233de65172bf69466757261f48b8981b6
Mavrick91 <ray.mavrick@gmail.com>
2024-03-09 08:59:15 +0800

    Optimize import path and simplify prefetchInfiniteQuery in Videos/index.tsx.

--- src/app/channel/_components/TabComponents/Videos/index.tsx
+++ src/app/channel/_components/TabComponents/Videos/index.tsx
@@ -2,7 +2,7 @@ import MaxWidthWrapper from '~/components/MaxWidthWrapper';
 import { getActivitiesDetails, GetActivitiesDetailsReturn } from '~/services/activityService';
 import getQueryClient from '~/getQueryClient';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import ChannelVideos from '~/app/channel/_components/TabComponents/Videos/ChannelVideos';
+import ChannelVideos from './ChannelVideos';
 
 type Props = {
   channelId: string;
@@ -13,7 +13,7 @@ async function VideoPage({ channelId }: Props) {
 
   await queryClient.prefetchInfiniteQuery({
     queryKey: ['channelActivities', channelId],
-    queryFn: async ({ pageParam = '' }) => getActivitiesDetails({ channelId, maxResults: 50, pageToken: pageParam }),
+    queryFn: async () => getActivitiesDetails({ channelId, maxResults: 50 }),
     getNextPageParam: (lastPage: GetActivitiesDetailsReturn) => lastPage.nextPageToken ?? undefined,
     initialPageParam: '',
   });
